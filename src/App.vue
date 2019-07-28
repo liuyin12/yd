42ce504fb6ec530395c3fa8712b5d8706a4afbf9
ly <1990707901@qq.com>
2019-07-28 16:31:38 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>app
+    <van-button type="default">默认按钮</van-button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
